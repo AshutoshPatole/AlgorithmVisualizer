6f0c3cdc6a589a8136a116423aa0b7e4851c0fb6
AshutoshPatole <apatole479@gmail.com>
2020-11-15 11:26:59 +0530

    index.js added with better class and id names

---
+++ scripts/index.js
@@ -0,0 +1,62 @@
+// accessing html elements using by their ID
+
+let arraySizeSlider = document.getElementById('array_size');
+let generateArrayButton = document.getElementById("generate_array");
+let sortingSpeed = document.getElementById("sorting_speed");
+let algorithmButtons = document.querySelectorAll(".algos button");
+let container = document.getElementById("array_container");
+
+/* 
+    we will create 2 arrays here
+        ==> div_sizes[0..n] will store the heights of the respected div(bar)
+        ==> divs[0..n] will store the elements i.e a div
+*/
+let div_sizes = [];
+let divs = [];
+let margin_size;
+
+// arraySize holds the value/input from the slider
+let arraySize = arraySizeSlider.value;
+
+// Event Listeners
+generateArrayButton.addEventListener("click", generateArray);
+arraySizeSlider.addEventListener("input", updateArraySize);
+
+/* 
+    generateArray function 
+        ==> it creates random heights for the bars using maximum and minimum value of the slider and height of the page,
+            it then stores the generated random heights in the div_sizes array[0..n]
+        ==> the divs[] array now creates div element and store them,
+            we then append those divs to the container area
+        ==> if the arraySize is more than 50 we will not show the height inside the div & at last we did some styling for each bar
+*/
+function generateArray() {
+    container.innerHTML = "";
+    for (let i = 0; i < arraySize; i++) {
+        div_sizes[i] = Math.floor(Math.random() * 0.70 * (arraySizeSlider.max - arraySizeSlider.min)) + 10;
+        divs[i] = document.createElement("div");
+        container.appendChild(divs[i]);
+        margin_size = 0.15;
+        // console.log(`this is size of array : ${arraySize}`);
+        if (arraySize < 50) {
+            divs[i].innerHTML = div_sizes[i];
+        }
+        divs[i].style = " margin:0% " + margin_size + "%; background-image:linear-gradient(#614385, #3859B2); width:" + (100 / arraySize - (2 * margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+    }
+}
+
+// this function is called whenever user changes arraySize slider
+function updateArraySize() {
+    arraySize = arraySizeSlider.value;
+    generateArray();
+}
+window.onload = updateArraySize();
+
+for (let i = 0; i < algorithmButtons.length; i++) {
+    algorithmButtons[i].addEventListener("click", startSortingAlgorithm);
+}
+
+function startSortingAlgorithm() {
+    // TODO write a switch case to change algorithms
+
+}
